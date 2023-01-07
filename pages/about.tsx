a25c24572f6fd8c496fcdcbe26b175973c3d6f17
jacquelinepayne-jpg <jacquelinepayne40@gmail.com>
2023-01-07 17:56:11 -0500

    smol update on text

--- pages/about.tsx
+++ pages/about.tsx
@@ -2,8 +2,9 @@ import type { NextPage } from 'next';
 
 const About = () => {
     return (
-        <div>
-            <h1>If you are reading this, there is a good chance you are one of the lucky few to be able to live a good life. The chance of you being here was incredibly small. Contine to find out how small.</h1>
+        <div className="about-header">
+            <h1>If you are reading this, there is a good chance you are one of the lucky few... 
+            </h1>
         </div>
     );
 }
